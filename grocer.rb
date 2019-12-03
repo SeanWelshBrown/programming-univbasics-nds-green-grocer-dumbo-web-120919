25ff2eecd121f735c435fb0485bc5e1a9c1468e8
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-03 14:19:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -32,7 +32,7 @@ def apply_coupons(cart, coupons)
     coupon = coupons[index]
     item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
     item_was_found = !!item_with_coupon
-    enough_for_coupon = item_was_found && item_with_coupon[]
+    enough_for_coupon = item_was_found && item_with_coupon[:count] >= 
 end
 
 def apply_clearance(cart)
